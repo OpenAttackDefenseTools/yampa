a1fdf0c1afeba3620f43e7efce86b9548152f03e
ShadowCone <git@shadowcone.com>
2022-11-18 16:15:44 +0100

    Move http/https tests into a single parametrized fixture

--- test/client/tests/conftest.py
+++ test/client/tests/conftest.py
@@ -29,17 +29,15 @@ def hostname(is_filtered):
     return "10.2.3.4" if is_filtered else "testserver"  # if os.environ["YAMP_TEST_CLIENT"] == "outside" else "10.2.3.4"
 
 
-@pytest.fixture(scope="session")
-def http_session(hostname):
-    with BaseUrlSession(base_url=f"http://{hostname}") as session:
-        yield session
-
-
-@pytest.fixture(scope="session")
-def https_session(hostname):
-    with tempfile.NamedTemporaryFile("w") as certfile:
-        certfile.write(os.environ["HTTPS_CERTIFICATE"])
-        certfile.flush()
-        with BaseUrlSession(base_url=f"https://{hostname}") as session:
-            session.verify = certfile.name
+@pytest.fixture(scope="session", params=["http", "https"])
+def http_session(request, hostname):
+    if request.param == "http":
+        with BaseUrlSession(base_url=f"http://{hostname}") as session:
             yield session
+    else:
+        with tempfile.NamedTemporaryFile("w") as certfile:
+            certfile.write(os.environ["HTTPS_CERTIFICATE"])
+            certfile.flush()
+            with BaseUrlSession(base_url=f"https://{hostname}") as session:
+                session.verify = certfile.name
+                yield session
